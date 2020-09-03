8f30542628d0aa0849c29fd060a2f16c6d6cea6f
Grønne <kegr.skp@edu.mercantec.dk>
2020-09-03 10:06:37 +0200

    why do I have to opush more

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 
 function getCity(){
-    var cityName = document.getElementById("inputField").value
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b10abfb32bd431f2017e24a855136&units=metric`
+    var cityName = document.getElementById("inputField").value;
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=415b10abfb32bd431f2017e24a855136&units=metric`;
 
-    var promise = fetch(url)
+    var promise = fetch(url);
 
-    var promise2 = promise.then(data=>data.json())
-    promise2.then(data=>console.log(data))
+    var promise2 = promise.then(data=>data.json());
+    promise2.then(data=>console.log(data));
 
-    promise2.then(data=>console.log(data.main.temp))
+    promise2.then(data=>something(data));
+}
 
-    
+function something(data){
+    document.getElementById("tempText").innerText = data.main.temp
 }
