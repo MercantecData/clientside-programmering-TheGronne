3e831469cd41caba9848c3e468e26a02238fa017
Grønne <kegr.skp@edu.mercantec.dk>
2020-09-07 07:13:42 +0200

    Det som jeg lavede i fredags (Glemte at pushe)

--- script.js
+++ script.js
@@ -11,6 +11,22 @@ function getCity(){
     promise2.then(data=>something(data));
 }
 
+function getCity2(){
+    var cityName = document.getElementById("inputField2").value;
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=415b10abfb32bd431f2017e24a855136&units=metric`;
+
+    var promise = fetch(url);
+
+    var promise2 = promise.then(data=>data.json());
+    promise2.then(data=>console.log(data));
+
+    promise2.then(data=>something2(data));
+}
+
 function something(data){
     document.getElementById("tempText").innerText = data.main.temp
+}
+
+function something2(data){
+    document.getElementById("tempText2").innerText = data.main.temp
 }
