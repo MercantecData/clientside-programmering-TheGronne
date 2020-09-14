4faef71735c7f1f71d481b981f418501f6e3b059
Grønne <kegr.skp@edu.mercantec.dk>
2020-09-14 07:40:14 +0200

    Det jeg ikke kunne pushe pga. min pc døde

--- .vscode/script.js
+++ .vscode/script.js
@@ -2,6 +2,7 @@
 function getCity(){
     var cityName = document.getElementById("inputField").value;
     var apiKey = '415b10abfb32bd431f2017e24a855136'
+
     var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
     var promise = fetch(url);
@@ -10,28 +11,34 @@ function getCity(){
     promise2.then(data=>console.log(data));
 
     promise2.then(data=>something(data));
+
+
+
+    var url2 = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
+
+    var hourlyPromise = fetch(url2);
+
+    var hourlyPromise2 = hourlyPromise.then(data2=>data2.json());
+    hourlyPromise2.then(data2=>console.log(data2));
+
+    hourlyPromise2.then(data2=>something2(data2));
+    
+    
 }
-
-function getCity2(){
-    var cityName = document.getElementById("inputField2").value;
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=415b10abfb32bd431f2017e24a855136&units=metric`;
-
-    var promise = fetch(url);
-
-    var promise2 = promise.then(data=>data.json());
-    promise2.then(data=>console.log(data));
-
-    promise2.then(data=>something2(data));
-}
-
 function something(data){
+    document.getElementById('header').innerText = 'Monday'
     document.getElementById("tempText2").innerText = data.main.temp
     document.getElementById("weatherText2").innerText = data.weather[0].main
     document.getElementById("countryText2").innerText = data.sys.country
-    var object = document.createElement("form")
-    object = document.forms['form']['formName'].value
 }
 
-function something2(data){
-    document.getElementById("tempText2").innerText = data.main.temp
+function something2(data2){
+    var idk = document.getElementById('form')
+    for (var i = 0; i < data2.list.length; i++)
+    {
+        listItem = document.createElement('h1')
+        listItem.innerText = data2.list[i].dt_txt
+
+        idk.appendChild(listItem)
+    }
 }
