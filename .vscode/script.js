2b5b21d052569ded8645a1a407be7a22f6132d35
Grønne <kegr.skp@edu.mercantec.dk>
2020-10-05 10:51:18 +0200

    Pokemon Changes

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,42 +1,18 @@
 
-function getCity(){
-    var cityName = document.getElementById("inputField").value;
-    var apiKey = '415b10abfb32bd431f2017e24a855136'
+function getPokemon(){
+    var pokeName = document.getElementById("inputField").value;
 
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    var url = `https://pokeapi.co/api/v2/pokemon/${pokeName.toLowerCase()}/`;
 
-    var promise = fetch(url);
+    fetch(url).then(res => res.json()).then(res => {
+        console.log(res);
 
+        var picURL = res.sprites.front_default;
+        document.getElementById("picture").src = picURL
 
-    promise2.then(data=>something(data));
-
-
-
-    var url2 = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
-
-    var hourlyPromise = fetch(url2);
-
-    var hourlyPromise2 = hourlyPromise.then(data2=>data2.json());
-    hourlyPromise2.then(data2=>console.log(data2));
-
-    hourlyPromise2.then(data2=>something2(data2));
-    
-    
-}
-function something(data){
-    document.getElementById('header').innerText = 'Monday'
-    document.getElementById("tempText2").innerText = data.main.temp
-    document.getElementById("weatherText2").innerText = data.weather[0].main
-    document.getElementById("countryText2").innerText = data.sys.country
-}
-
-function something2(data2){
-    var idk = document.getElementById('form')
-    for (var i = 0; i < data2.list.length; i++)
-    {
-        listItem = document.createElement('h1')
-        listItem.innerText = data2.list[i].dt_txt
-
-        idk.appendChild(listItem)
-    }
+        document.getElementById("type1").innerText = res.types[0].type.name
+        if (res.types.length > 1){
+            document.getElementById("type2").innerText = res.types[1].type.name
+        }
+    })
 }
