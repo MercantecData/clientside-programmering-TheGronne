35c6b83af3c4905044d9b403d28afee4355b045a
Grønne <kegr.skp@edu.mercantec.dk>
2020-10-06 11:47:52 +0200

    Stats and name

--- .vscode/script.js
+++ .vscode/script.js
@@ -11,94 +11,28 @@ function getPokemon(){
         var picURL = res.sprites.front_default;
         document.getElementById("picture").src = picURL
 
-        document.getElementById("type1").innerText = res.types[0].type.name
-        urltype1 = `https://pokeapi.co/api/v2/type/${res.types[0].type.name.toLowerCase()}/`;
+        document.getElementById("name").innerText = "Name: " + res.species.name
+
+        
+
+        document.getElementById("type1").innerText = "Type 1: " + res.types[0].type.name
 
         if (res.types.length > 1){
-            document.getElementById("type2").innerText = res.types[1].type.name
-            urltype2 = `https://pokeapi.co/api/v2/type/${res.types[1].type.name.toLowerCase()}/`;
+            document.getElementById("type2").innerText = "Type 2: " + res.types[1].type.name
+        } else {
+            document.getElementById("type2").innerText = ""
         }
 
-
-        fetch(urltype1).then(res2 => res2.json()).then(res2 => {
-            console.log(res2);
-    
-            var weaknesses;
-            var weaknesses2;
-
-            var resistances;
-            var resistances2;
-
-            var immunities;
-            var immunities2;
-            
-            weaknesses = res2.damage_relations.double_damage_from
-            resistances = res2.damage_relations.half_damage_from
-            immunities = res2.damage_relations.no_damage_from
-            console.log(weaknesses)
-            fetch(urltype2).then(res3 => res3.json()).then(res3 => {
-                console.log(res3);
-                
-                weaknesses2 = res3.damage_relations.double_damage_from
-                resistances2 = res3.damage_relations.half_damage_from
-                immunities2 = res3.damage_relations.no_damage_from
-                console.log(weaknesses2)
-    
-            })
-            
-            
-        })
+        document.getElementById("hp").innerText = "Healh: " + res.stats[0].base_stat
+        document.getElementById("attack").innerText = "Attack: " + res.stats[1].base_stat
+        document.getElementById("defense").innerText = "Defense: " + res.stats[2].base_stat
+        document.getElementById("specialAttack").innerText = "Special Attack: " + res.stats[3].base_stat
+        document.getElementById("specialDefense").innerText = "Special Defense: " + res.stats[4].base_stat
+        document.getElementById("speed").innerText = "Speed: " + res.stats[5].base_stat
+        
     })
-
-    
-
-    
 }
 
-function getPokemon2(){
-    var pokeName = document.getElementById("inputField2").value;
-
-    var url = `https://pokeapi.co/api/v2/pokemon/${pokeName.toLowerCase()}/`;
-    var urltype1
-    var urltype2
-    fetch(url).then(res => res.json()).then(res => {
-        console.log(res);
-
-        var picURL = res.sprites.front_default;
-        document.getElementById("picture2").src = picURL
-        
-        document.getElementById("type12").innerText = res.types[0].type.name
-        urltype1 = `https://pokeapi.co/api/v2/type/${res2.types[0].type.name.toLowerCase()}/`;
-
-        if (res.types.length > 1){
-            document.getElementById("type22").innerText = res.types[1].type.name
-            urltype2 = `https://pokeapi.co/api/v2/type/${res3.types[1].type.name.toLowerCase()}/`;
-        }
-
-        var weaknesses;
-        var weaknesses2;
-
-        fetch(urltype1).then(res2 => res2.json()).then(res2 => {
-            console.log(res2);
-    
-            
-            
-            weaknesses = new array(res2.damage_relations.double_damage_from)
-            console.log("yo")   
-            console.log(weaknesses)
-            fetch(urltype2).then(res3 => res3.json()).then(res3 => {
-                console.log(res3);
-                
-                weaknesses2 = new array(res3.damage_relations.double_damage_from)
-    
-    
-            })
-            
-            
-        })
-    })
-
-    
-
-    
+function capitalize(c){
+    return capitalize(c)
 }
